7fb9ba779ebf6587f4a5a61ed38955f1a98735f1
cristian1murgu <cmurgu@hotmail.com>
2019-02-28 14:54:34 -0800

    Revert "initial commit"
    This reverts commit 4b2c916de9f2980910f21391d2861814e18013e0.

--- _vti_cnf/animate.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_author:SR|Christian
-vti_modifiedby:SR|Christian
-vti_timecreated:TR|18 Feb 2007 22:43:13 -0000
-vti_timelastmodified:TR|18 Feb 2007 22:43:13 -0000
-vti_cacheddtm:TX|18 Feb 2007 22:43:13 -0000
-vti_filesize:IR|14261
-vti_extenderversion:SR|4.0.2.8912
-vti_backlinkinfo:VX|
--- _vti_cnf/mm_menu.js
+++
@@ -1,6 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|26 Apr 2002 23:21:56 -0000
-vti_extenderversion:SR|4.0.2.8912
-vti_cacheddtm:TX|26 Apr 2002 23:21:56 -0000
-vti_filesize:IR|30737
-vti_backlinkinfo:VX|battery\\ disconnect.htm cmbc_alternator_flash.htm nf\\ curb\\ light.htm serv01.htm starter1.htm starter_flash.htm nf\\ battery\\ disconnect\\ sw\\ &\\ fuses.htm battery_box_nf.htm nf\\ battery\\ box.htm nf\\ exterior\\ lamps\\ layout.htm starter.htm exterior_lights.htm starter_flash.html
